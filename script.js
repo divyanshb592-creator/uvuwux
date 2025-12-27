560208dad37a292bb8ce61df67dc0a6301aeae46
divyanshb592-creator <divyanshb592@gmail.com>
2025-12-27 11:46:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -723,12 +723,4 @@ window.addEventListener('load', function() {
 });
 
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0P627EVFEC"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-0P627EVFEC');
-</script>
